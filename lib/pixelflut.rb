3f76d496e8bc4af68dfe8ceffabc7839dd04c413
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-03-08 09:00:20 +0100

    Use different RMagick load approach

--- lib/pixelflut.rb
+++ lib/pixelflut.rb
@@ -3,5 +3,6 @@ module Pixelflut
   autoload :App, File.join(LibDir, 'app.rb')
   autoload :Server, File.join(LibDir, 'server.rb')
   autoload :Canvas, File.join(LibDir, 'canvas.rb')
+  autoload :Converter, File.join(LibDir, 'converter.rb')
   autoload :VERSION, File.join(LibDir, 'version.rb')
 end
--- lib/pixelflut/converter.rb
+++ lib/pixelflut/converter.rb
@@ -3,50 +3,51 @@
 module Pixelflut
   class Converter
     Error = Class.new(RuntimeError)
+  end
 
-    attr_accessor :x_offset, :y_offset
+  begin
+    require 'rmagick'
 
-    def initialize(file_name)
-      load_rmagick
-      @images = load_image(file_name)
-      @x_offset = @y_offset = 0
-    end
+    class Converter
+      Avail = true
+      attr_accessor :x_offset, :y_offset
 
-    def resize_to(width, height = nil)
-      @images.each{ |image| image.resize_to_fit!(width, height) }
-    end
+      def initialize(file_name)
+        @images = load_images(file_name)
+        @x_offset = @y_offset = 0
+      end
 
-    def each_pixel
-      return to_enum(__method__) unless block_given?
-      @images.each do |image|
-        image.each_pixel do |color, x, y|
-          yield(
-            x + @x_offset,
-            y + @y_offset,
-            color.to_color(Magick::AllCompliance, true, 8, true)[1, 8]
-          ) unless 0xffff == color.opacity
+      def resize_to(width, height = nil)
+        @images.each{ |image| image.resize_to_fit!(width, height) }
+      end
+
+      def each_pixel
+        return to_enum(__method__) unless block_given?
+        @images.each do |image|
+          image.each_pixel do |color, x, y|
+            yield(
+              x + @x_offset,
+              y + @y_offset,
+              color.to_color(Magick::AllCompliance, true, 8, true)[1, 8]
+            ) unless 0xffff == color.opacity
+          end
         end
       end
+
+      def each_line
+        return to_enum(__method__) unless block_given?
+        each_pixel{ |x, y, rgba| yield "PX #{x} #{y} #{rgba}" }
+      end
+
+      private
+
+      def load_images(file_name)
+        Magick::ImageList.new(file_name)
+      rescue Magick::ImageMagickError => e
+        raise(Error, e.message)
+      end
     end
-
-    def each_line
-      return to_enum(__method__) unless block_given?
-      each_pixel{ |x, y, rgba| yield "PX #{x} #{y} #{rgba}" }
-    end
-
-    private
-
-    def load_image(file_name)
-      Magick::ImageList.new(file_name)
-    rescue Magick::ImageMagickError => e
-      raise(Error, e.message)
-    end
-
-    def load_rmagick
-      require 'rmagick'
-    rescue LoadError
-      raise(Error, 'unable to load gem - "RMagick" not installed')
-    end
-
+  rescue LoadError
+    Converter::Avail = false
   end
 end
